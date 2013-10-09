00360049f84815f8646286db90ded7adbf9fd1cc
Darren Liew <ldarren@gmail.com>
2013-10-09 09:05:11 +0800

    copy n pasted all google iab steps

--- src/com/baroq/pico/GoogleInAppBilling.java
+++ src/com/baroq/pico/GoogleInAppBilling.java
@@ -44,4 +44,106 @@ public class GoogleInAppBilling extends Plugin {
             unbindService(mServiceConn);
         }   
     }
+
+    public void getSKUDetails(){
+        ArrayList skuList = new ArrayList();
+        skuList.add("premiumUpgrade");
+        skuList.add("gas");
+        Bundle querySkus = new Bundle();
+        querySkus.putStringArrayList(“ITEM_ID_LIST”, skuList);
+
+        Bundle skuDetails = mService.getSkuDetails(3, getPackageName(), "inapp", querySkus);
+
+        int response = skuDetails.getInt("RESPONSE_CODE");
+        if (response == 0) {
+            ArrayList responseList = skuDetails.getStringArrayList("DETAILS_LIST");
+
+            for (String thisResponse : responseList) {
+                JSONObject object = new JSONObject(thisResponse);
+                String sku = object.getString("productId");
+                String price = object.getString("price");
+                if (sku.equals("premiumUpgrade")) mPremiumUpgradePrice = price;
+                else if (sku.equals("gas")) mGasPrice = price;
+            }
+        }
+    }
+
+    public void itemPurchase(String sku){
+        Bundle buyIntentBundle = mService.getBuyIntent(3, getPackageName(), sku, "inapp", "bGoa+V7g/yqDXvKRqq+JTFn4uQZbPiQJo4pf9RzJ");
+        switch(buyIntentBundle.getInt("RESPONSE_CODE", 9)){
+            case 0: // BILLING_RESPONSE_RESULT_OK
+                PendingIntent pendingIntent = buyIntentBundle.getParcelable("BUY_INTENT");
+                startIntentSenderForResult(pendingIntent.getIntentSender(), 1001, new Intent(), Integer.valueOf(0), Integer.valueOf(0), Integer.valueOf(0));
+                break;
+            case 1: // BILLING_RESPONSE_RESULT_USER_CANCELED 
+                break;
+            case 3: // BILLING_RESPONSE_RESULT_BILLING_UNAVAILABLE 
+                break;
+            case 4: // BILLING_RESPONSE_RESULT_ITEM_UNAVAILABLE 
+                break;
+            case 5: // BILLING_RESPONSE_RESULT_DEVELOPER_ERROR 
+                break;
+            case 6: // BILLING_RESPONSE_RESULT_ERROR 
+                break;
+            case 7: // BILLING_RESPONSE_RESULT_ITEM_ALREADY_OWNED 
+                break;
+            case 8: // BILLING_RESPONSE_RESULT_ITEM_NOT_OWNED 
+                break;
+            case 9: // BILLING_RESPONSE_RESULT_OK
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) { 
+        if (requestCode == 1001) {           
+            int responseCode = data.getIntExtra("RESPONSE_CODE", 0);
+            String purchaseData = data.getStringExtra("INAPP_PURCHASE_DATA");
+            String dataSignature = data.getStringExtra("INAPP_DATA_SIGNATURE");
+
+            if (resultCode == RESULT_OK) {
+                try {
+                    JSONObject jo = new JSONObject(purchaseData);
+                    String sku = jo.getString("productId");
+                    alert("You have bought the " + sku + ". Excellent choice, 
+                    adventurer!");
+                }
+                catch (JSONException e) {
+                    alert("Failed to parse purchase data.");
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void getPurchase(){
+        Bundle ownedItems = mService.getPurchases(3, getPackageName(), "inapp", null);
+        int response = ownedItems.getInt("RESPONSE_CODE");
+        if (response == 0) {
+            ArrayList ownedSkus = 
+            ownedItems.getStringArrayList("INAPP_PURCHASE_ITEM_LIST");
+            ArrayList purchaseDataList = 
+            ownedItems.getStringArrayList("INAPP_PURCHASE_DATA_LIST");
+            ArrayList signatureList = 
+            ownedItems.getStringArrayList("INAPP_DATA_SIGNATURE");
+            String continuationToken = 
+            ownedItems.getString("INAPP_CONTINUATION_TOKEN");
+
+            for (int i = 0; i < purchaseDataList.size(); ++i) {
+                String purchaseData = purchaseDataList.get(i);
+                String signature = signatureList.get(i);
+                String sku = ownedSkus.get(i);
+
+                // do something with this purchase information
+                // e.g. display the updated list of products owned by user
+            } 
+
+            // if continuationToken != null, call getPurchases again 
+            // and pass in the token to retrieve more items
+        }
+    }
+
+    public void consumeItem(){
+        int response = mService.consumePurchase(3, getPackageName(), token);
+    }
 };
