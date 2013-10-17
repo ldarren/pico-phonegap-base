53044fc514e8f5231e9183d21e726657c2a4a552
Darren Liew <ldarren@gmail.com>
2013-10-17 23:19:56 +0800

    refactor pico-google-plugin, added google services jar

--- assets/www/pico-plugin-goog.js
+++ assets/www/pico-plugin-goog.js
@@ -1,48 +1,50 @@
 var cordova = window.cordova || window.Cordova;
-window.GOOG = {
-    iabOpen: function(apiKey, cb) {
-      cordova.exec(
-          function(){ cb(); },
-          function(err){ cb(err); },
-          'InAppBilling', 'iabOpen', [apiKey]);
-    },
-    iabClose: function(cb) {
-      cordova.exec(
-          function(){ if (cb) cb(); },
-          function(err){ if (cb) cb(err); },
-          'InAppBilling', 'iabClose', []);
-    },
-    iabInventory: function(skus, cb){
-      cordova.exec(
-          function(inventory){ if (cb) cb(null, inventory); },
-          function(err){ if (cb) cb(err); },
-          'InAppBilling', 'iabInventory', skus || []);
-    },
-    iabBuy: function(sku, payload, cb){
-      cordova.exec(
-          function(inventory){ if (cb) cb(null, inventory); },
-          function(err){ if (cb) cb(err); },
-          'InAppBilling', 'iabBuy', [sku, payload]);
-    },
-    iabSubscribe: function(sku, payload, cb){
-      cordova.exec(
-          function(inventory){ if (cb) cb(null, inventory); },
-          function(err){ if (cb) cb(err); },
-          'InAppBilling', 'iabSubscribe', [sku, payload]);
-    },
-    iabConsumes: function(purchaseList, cb){
-        var consume = function(inList, outList, index, cb){
-            if (inList.length >= index) return cb(null, outList);
-            cordova.exec(
-                function(out){ outList.push(out); consume(inList, outList, ++index, cb); },
-                function(err){ if (cb) cb(err, outList); },
-                'InAppBilling', 'iabConsume', [inList[index]]);
-        };
+window.gapi= {
+    iab: {
+        open: function(apiKey, cb) {
+          cordova.exec(
+              function(){ cb(); },
+              function(err){ cb(err); },
+              'InAppBilling', 'iabOpen', [apiKey]);
+        },
+        close: function(cb) {
+          cordova.exec(
+              function(){ if (cb) cb(); },
+              function(err){ if (cb) cb(err); },
+              'InAppBilling', 'iabClose', []);
+        },
+        inventory: function(skus, cb){
+          cordova.exec(
+              function(inventory){ if (cb) cb(null, inventory); },
+              function(err){ if (cb) cb(err); },
+              'InAppBilling', 'iabInventory', skus || []);
+        },
+        buy: function(sku, payload, cb){
+          cordova.exec(
+              function(inventory){ if (cb) cb(null, inventory); },
+              function(err){ if (cb) cb(err); },
+              'InAppBilling', 'iabBuy', [sku, payload]);
+        },
+        subscribe: function(sku, payload, cb){
+          cordova.exec(
+              function(inventory){ if (cb) cb(null, inventory); },
+              function(err){ if (cb) cb(err); },
+              'InAppBilling', 'iabSubscribe', [sku, payload]);
+        },
+        consumes: function(purchaseList, cb){
+            var _c = function(inList, outList, index, cb){
+                if (inList.length >= index) return cb(null, outList);
+                cordova.exec(
+                    function(out){ outList.push(out); _c(inList, outList, ++index, cb); },
+                    function(err){ if (cb) cb(err, outList); },
+                    'InAppBilling', 'iabConsume', [inList[index]]);
+            };
 
-        if (!purchaseList || !purchaseList.length){
-            return cb(purchaseList);
-        }else{
-            consume(purchaseList, [], 0, cb);
+            if (!purchaseList || !purchaseList.length){
+                return cb(purchaseList);
+            }else{
+                _c(purchaseList, [], 0, cb);
+            }
         }
     }
 };
